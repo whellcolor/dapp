25f1efa48732c7f26fbe71a603d4db5a28c8be18
whellcolor <bahansatekambing@gmail.com>
2026-05-07 07:06:27 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,3 +15,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </QueryClientProvider>
   </WagmiProvider>
 )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
