57f7e0eb3314a7e6e95b221eb432730f9d3e63f4
whellcolor <bahansatekambing@gmail.com>
2026-05-08 20:23:52 +0700

    Create walletTool.js

---
+++ mcp/tools/walletTool.js
@@ -0,0 +1,11 @@
+export const walletInfoTool = {
+  name: "wallet_info",
+
+  description:
+    "Get server wallet information",
+
+  inputSchema: {
+    type: "object",
+    properties: {},
+  },
+};
