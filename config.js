dab8d1ecefb2b993704e8ef9553adc802903e4cd
whellcolor <bahansatekambing@gmail.com>
2026-05-09 11:02:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+export const CONFIG = {
+  RPC_URL: "https://1.rpc.thirdweb.com/e6230839e78c90a670b64b1f26c0b3d8",
+
+  CONTRACT_ADDRESS: "0xd8519A8b8825Aa0DcC73aAD572f447FAE102fe88",
+
+  CHAIN_ID: 17000
+};
