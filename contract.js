d0dcdd4c7277f40fb8d4144d1cb45589ccfa950d
whellcolor <bahansatekambing@gmail.com>
2026-05-09 11:03:07 +0700

    Create contract.js

---
+++ contract.js
@@ -0,0 +1,10 @@
+// contract.js
+
+export const ABI = [
+  "function startMining()",
+  "function stopMining()",
+  "function claimReward()",
+  "function mint(address to, string uri)",
+  "function withdraw(uint256 amount)",
+  "function getPendingReward(address user) view returns (uint256)"
+];
