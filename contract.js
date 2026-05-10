55c8585e6b01cfaa2ecb3eb904f0242ddaa99bb5
whellcolor <bahansatekambing@gmail.com>
2026-05-10 09:25:17 +0700

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,3 +1,21 @@
+import { ethers } from "ethers";
+
+export const TOKEN_ADDRESS =
+  "0xd8519a8b8825aa0dcc73aad572f447fae102fe88";
+
+export const FAUCET_ADDRESS =
+  "0xd8519a8b8825aa0dcc73aad572f447fae102fe88";
+
+export const TOKEN_ABI = [
+  "function balanceOf(address) view returns (uint256)",
+];
+
+export const FAUCET_ABI = [
+  "function requestTokens()",
+  "function canClaim(address) view returns (bool)",
+  "function remainingAllowance(address) view returns (uint256)",
+];
+
 // contract.js
 
 export const ABI = [
