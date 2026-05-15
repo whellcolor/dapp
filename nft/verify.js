5a1cce670c05ae554ae321956790f2fbcdfed1a0
whellcolor <bahansatekambing@gmail.com>
2026-05-15 11:15:07 +0700

    Create verify.js

---
+++ nft/verify.js
@@ -0,0 +1,10 @@
+const nftVerification = {
+  network: "Ethereum",
+  wallet: "0xd8519A8b8825Aa0DcC73aAD572f447FAE102fe88",
+  image: "https://whellcolor.github.io/dapp/awdevNFT.jpg",
+  dapp: "https://whellcolor.github.io/dapp/nft/",
+  statement: `[Etherscan.io dd/mm/yyyy hh:mm:ss] I, [0xd8519A8b8825Aa0DcC73aAD572f447FAE102fe88], hereby verify that I am the owner/creator of the token contract address [bridged token's contract address] connect on url dapp mint original contracttor https://whellcolor.github.io/dapp/nft/`
+};
+
+console.log("NFT Verification");
+console.log(nftVerification);
